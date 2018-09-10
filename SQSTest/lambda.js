5ce41ce6ae37ada0fcffdf506e3a561958b9d189
sigmaidetest <cehib@balanc3r.com>
2018-09-10 15:25:13 +0530

    init

---
+++ SQSTest/lambda.js
@@ -0,0 +1,30 @@
+let AWS = require('aws-sdk');
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
+
+exports.handler = function (event, context, callback) {
+
+    sqs.receiveMessage({
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/318300609668/MyNewQueue',
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: '1',
+        VisibilityTimeout: '30',
+        WaitTimeSeconds: '0'
+    }).promise()
+        .then(receivedMsgData => {
+            if (!!(receivedMsgData) && !!(receivedMsgData.Messages)) {
+                let receivedMessages = receivedMsgData.Messages;
+                receivedMessages.forEach(message => {
+                    // your logic to access each message through out the loop. Each message is available under variable message 
+                    // within this block
+                });
+            } else {
+                // No messages to process
+            }
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+    callback(null, 'Successfully executed');
+}
